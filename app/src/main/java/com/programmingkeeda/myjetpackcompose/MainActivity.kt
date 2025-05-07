b8a9cdb7d3ce3a258e2ec9a4bbadacbb5ad2a365
Saurabh Kharade <saurabhkharade2@gmail.com>
2025-05-07 22:50:14 +0530

    TextView Demo

--- app/src/main/java/com/programmingkeeda/myjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/programmingkeeda/myjetpackcompose/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MyJetpackComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name =  "Jetpack Compose Android Developer!",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
