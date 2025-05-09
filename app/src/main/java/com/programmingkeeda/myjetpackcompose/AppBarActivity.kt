a51e2609845e7fc9fc481223bc140204bc139243
Saurabh Kharade <saurabhkharade2@gmail.com>
2025-05-09 22:48:53 +0530

    Appbar Demo

---
+++ app/src/main/java/com/programmingkeeda/myjetpackcompose/AppBarActivity.kt
@@ -0,0 +1,36 @@
+package com.programmingkeeda.myjetpackcompose
+
+import android.os.Bundle
+import android.provider.CalendarContract.Colors
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.programmingkeeda.myjetpackcompose.ui.theme.MyJetpackComposeTheme
+
+class AppBarActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyJetpackComposeTheme {
+                Scaffold(
+                    topBar = { TopAppBar(title = { "My Jetpack Compose" }) },
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    Greeting(
+                        name = "Android", modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
