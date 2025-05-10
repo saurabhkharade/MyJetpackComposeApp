c4b1a5fc6419557c9f7741e37f98029409aadb80
Saurabh Kharade <saurabhkharade2@gmail.com>
2025-05-10 11:44:02 +0530

    Appbar

--- app/src/main/java/com/programmingkeeda/myjetpackcompose/AppBarActivity.kt
+++ app/src/main/java/com/programmingkeeda/myjetpackcompose/AppBarActivity.kt
@@ -1,15 +1,21 @@
 package com.programmingkeeda.myjetpackcompose
 
 import android.os.Bundle
-import android.provider.CalendarContract.Colors
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.programmingkeeda.myjetpackcompose.ui.theme.MyJetpackComposeTheme
@@ -22,12 +28,26 @@ class AppBarActivity : ComponentActivity() {
         setContent {
             MyJetpackComposeTheme {
                 Scaffold(
-                    topBar = { TopAppBar(title = { "My Jetpack Compose" }) },
-                    modifier = Modifier.fillMaxSize()
+                    topBar = {
+                        TopAppBar(
+                            title = { Text("Learn Jetpack") },
+                            colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = Color.Red,
+                                titleContentColor = Color.White,
+                                actionIconContentColor = Color.White
+                            ),
+                            actions = {
+                                IconButton(onClick = {}) {
+                                    Icon(Icons.Default.MoreVert, contentDescription = "Settings")
+                                }
+                            },
+                            modifier = Modifier.padding()
+                        )
+                    }, modifier = Modifier.fillMaxSize()
                 ) { innerPadding ->
-                    Greeting(
-                        name = "Android", modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(modifier = Modifier.padding(innerPadding)) {
+                        Text("Android Developer")
+                    }
                 }
             }
         }
